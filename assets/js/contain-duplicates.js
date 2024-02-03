cf71db132251a672400f9d98255c74186901e1f3
Ramareddy <ramareddy9652@gmail.com>
2024-02-03 17:49:26 +0530

    done changes

--- assets/js/contain-duplicates.js
+++ assets/js/contain-duplicates.js
@@ -1,13 +1,23 @@
-var containsDuplicate = function(nums) {
-    let hTable = {};
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] in hTable)
-            return true;
-            hTable[nums[i]] = 1;
+function checkDuplicates() {
+    var inputArray = document.getElementById("inputArray").value;
+    var nums = inputArray.split(',').map(Number);
+
+    var hasDuplicates = checkForDuplicates(nums);
+
+    var resultElement = document.getElementById("result");
+    resultElement.innerText = hasDuplicates ? "Duplicates found!" : "No duplicates found.";
+  }
+
+  function checkForDuplicates(nums) {
+    var seen = {};
+
+    for (var i = 0; i < nums.length; i++) {
+      if (seen[nums[i]]) {
+        return true;
+      } else {
+        seen[nums[i]] = true;
+      }
     }
+
     return false;
-};
-
-let nums = [1,2,3,4];
-
-console.log(containsDuplicate(nums));
+  }
